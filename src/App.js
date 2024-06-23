84b238e8ab99d0b3aac35344aa4544909092a926
unknown <ozkannayben@gmail.com>
2024-06-23 14:09:42 -0700

    added cart feature

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
   return (
     <CartProvider>
       <Cart />
-      {cartIsShown && <div>Cart...</div>}
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
       <Header onShowCart={showCartHanlder} />
       <main>
         <Meals />
